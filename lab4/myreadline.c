7cbdfedaf16baed296c7e9aaaf86ea3897701938
NeArina <arina.adamyan.03@mail.ru>
2022-12-18 18:30:17 +0300

    remove defines

--- lab4/myreadline.c
+++ lab4/myreadline.c
@@ -1,6 +1,6 @@
 #include "myreadline.h"
-#define BUFFER_SIZE 20
-#define FORMAT_STR ("%19[^\n]s") // переделать scanf("%80["\n"], buf);
+// #define BUFFER_SIZE 20
+// #define FORMAT_STR ("%19[^\n]s") // переделать scanf("%80["\n"], buf);
 #include <stdlib.h>
 #include "mystrlen.h"
 #include <stdio.h>
@@ -8,9 +8,11 @@
 int myreadline(char *p_str, char **ppstr) {
     printf("%s", p_str);
 
-    int size = BUFFER_SIZE; //////buffer
+    //char buffer[20];
+
+    int size = 20; //////buffer
     int curSize = 0;
-    char *str = malloc(BUFFER_SIZE * sizeof(char));
+    char *str = malloc(20 * sizeof(char));
 
     if (str == NULL) {
         printf("Error of malloc in readline");
@@ -21,16 +23,17 @@ int myreadline(char *p_str, char **ppstr) {
     int str_ended = 0;
     int t;
     do {
+      
         // lfkjjhdjghjd25451248________________________
-        t = scanf(FORMAT_STR, str + curSize);
+        t = scanf("%19[^\n]", str + curSize);
         if (t == EOF) {
             *ppstr = str;
             return EOF;
         }
         str_ended = str[curSize] == 0;
         curSize += mystrlen(str + curSize);
-        if (curSize + BUFFER_SIZE >= size) {
-            size += BUFFER_SIZE;
+        if (curSize + 20 >= size) {
+            size += 20;
             str = realloc(str, sizeof(char) * size);
         }
 
