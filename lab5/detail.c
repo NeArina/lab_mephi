7017d3107a02d8d26c274d091dca90753e33cfb7
NeArina <arina.adamyan.03@mail.ru>
2022-12-24 18:24:08 +0300

    lab5 detail added

---
+++ lab5/detail.c
@@ -0,0 +1,51 @@
+#include "detail.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+detail *create_detail(const char *id, const char *name, int count) {
+  detail *result = (detail *)malloc(sizeof(detail));
+
+  if (!result) {
+    return NULL;
+  }
+
+  strncpy(result->id, id, 8);
+  (result->id)[8] = 0;
+
+  int name_len = strlen(name);
+  result->name = (char *)malloc(sizeof(char) * name_len + 1);
+  strncpy(result->name, name, name_len);
+  (result->name)[name_len] = 0;
+
+  result->count = count;
+
+  return result;
+}
+
+void free_detail(detail *det) {
+  free(det->name);
+  free(det);
+}
+
+void print_detail(detail *det) {
+  printf("Detail: id = %s, name = %s, count = %d\n", det->id, det->name,
+         det->count);
+}
+
+// comp	-	comparison function which returns ​a negative integer value if
+// the first argument is less than the second, a positive integer value if the
+// first argument is greater than the second and zero if the arguments are
+// equivalent.
+int cmp_detail_count_asc(const void *a, const void *b) {
+  detail *det_a = *(detail **)a;
+  detail *det_b = *(detail **)b;
+
+  if (det_a->count < det_b->count) {
+    return -1;
+  } else if (det_a->count > det_b->count) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
--- lab5/lab_5.c
+++ lab5/lab_5.c
@@ -1,57 +1,85 @@
-int main()
-{
-    // int stop = 0;
-    // int cmd = 0;
-    // while (!stop)
-    // {
-    //     printf("Commands:               \n \
+#include "detail.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  detail *d1 = create_detail("12345678", "Some Name", 25);
+  detail *d2 = create_detail("00001111", "A Name", 2);
+  detail *d3 = create_detail("98765432", "LSKF hkds", 10);
+
+  const int arr_size = 3;
+
+  detail **arr = calloc(arr_size, sizeof(detail *));
+  arr[0] = d1;
+  arr[1] = d2;
+  arr[2] = d3;
+
+  for (int i = 0; i < arr_size; i++) {
+    print_detail(arr[i]);
+  }
+
+  qsort(arr, arr_size, sizeof(detail *), cmp_detail_count_asc);
+
+  for (int i = 0; i < arr_size; i++) {
+    print_detail(arr[i]);
+  }
+
+  for (int i = 0; i < arr_size; i++) {
+    free_detail(arr[i]);
+  }
+
+  // int stop = 0;
+  // int cmd = 0;
+  // while (!stop)
+  // {
+  //     printf("Commands:               \n \
     // 0 - end the program               \n \
     // 1 - print current array           \n \
     // 2 - do the task and print results \n \
     // 3 - insert by index               \n \
     // 4 - delete by index               \n");
-    //     input_int(&cmd, 0, 4, "");
-    //     switch (cmd)
-    //     {
-    //     case 0:
-    //         stop = 1;
-    //         break;
-    //     case 1:
-    //         print_array(arr, size);
-    //         break;
-    //     case 2:
-    //         if (arr == NULL)
-    //         {
-    //             printf("array is empty, cannot execute my task\n");
-    //         }
-    //         else
-    //         {
-    //             new_arr = my_task(&arr, &size, &new_size);
-    //             printf("Original array, after:\n");
-    //             print_array(arr, size);
-    //             printf("\n");
-    //             printf("New array, after:\n");
-    //             print_array(new_arr, new_size);
-    //             printf("\n");
-    //         }
-    //         break;
-    //     case 3:
-    //         arr = insert_elem(arr, &size, &length);
-    //         break;
-    //     case 4:
-    //         if (arr == NULL)
-    //         {
-    //             printf("array is empty, cannot execute my task\n");
-    //         }
-    //         else
-    //         {
-    //             arr = delete_elem(arr, &size, &length);
-    //         }
-    //         break;
-    //     default:
-    //         printf("wrong cmd number\n");
-    //     }
-    // }
+  //     input_int(&cmd, 0, 4, "");
+  //     switch (cmd)
+  //     {
+  //     case 0:
+  //         stop = 1;
+  //         break;
+  //     case 1:
+  //         print_array(arr, size);
+  //         break;
+  //     case 2:
+  //         if (arr == NULL)
+  //         {
+  //             printf("array is empty, cannot execute my task\n");
+  //         }
+  //         else
+  //         {
+  //             new_arr = my_task(&arr, &size, &new_size);
+  //             printf("Original array, after:\n");
+  //             print_array(arr, size);
+  //             printf("\n");
+  //             printf("New array, after:\n");
+  //             print_array(new_arr, new_size);
+  //             printf("\n");
+  //         }
+  //         break;
+  //     case 3:
+  //         arr = insert_elem(arr, &size, &length);
+  //         break;
+  //     case 4:
+  //         if (arr == NULL)
+  //         {
+  //             printf("array is empty, cannot execute my task\n");
+  //         }
+  //         else
+  //         {
+  //             arr = delete_elem(arr, &size, &length);
+  //         }
+  //         break;
+  //     default:
+  //         printf("wrong cmd number\n");
+  //     }
+  // }
 
-    return 0;
+  return 0;
 }
